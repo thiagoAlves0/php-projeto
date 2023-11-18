871c549add2d8b624f2127cd7ab25c5b609cf001
GilbertoGS <gilbertogit@hotmail.com>
2023-11-18 12:20:17 -0300

    Merge branch 'main' of https://github.com/thiagoAlves0/php-projeto

--- projeto-aniflix/views/Contato.php
+++ projeto-aniflix/views/Contato.php
@@ -15,21 +15,23 @@
         <a class="nusetas" href="../views/index.php">←</a>
         <span class="nudata">Contato AniFlix</span>
     </div>
-<form action="/formulario-contato" method="post">
-    <div>
-        <label for="nome">Nome: </label>
-        <input type="texto" id="nome" name="nome_usuario">
-    </div>
-    <div>
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email_usuario">
-    </div>
-    <div>
-        <label for="texto">Texto:</label>
-        <textarea id="texto" name="texto_usuario"></textarea>
-    </div>
-</form>
-    
+    <form  id="contactForm" action="contatodb.php" method="post">
+        <div>
+            <label for="nome">Nome: </label>
+            <input type="text" id="nome" name="nome_usuario">
+        </div>
+        <div>
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email_usuario">
+        </div>
+        <div>
+            <label for="texto">Texto:</label>
+            <textarea id="texto" name="texto_usuario"></textarea>
+        </div>
+        <div>
+            <button type="submit">Enviar</button>
+        </div>
+    </form>
 </body>
 
 </html>
